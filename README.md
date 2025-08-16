8fc36cbcb5095df7fd95478a49055a5cc06c06d9
EiderMontalvo <sanchezmontalvoeider@gmail.com>
2025-08-15 23:31:17 -0500

    web finalizada y con correcion de errores

---
+++ README.md
@@ -0,0 +1,22 @@
+# amigo secreto
+
+aplicación web sencilla para sortear un "amigo secreto".  
+permite ingresar nombres de amigos, mostrarlos en una lista y elegir un ganador de forma aleatoria.
+
+---
+
+## funcionalidades
+- agregar amigos a una lista.
+- validar que el campo no esté vacío.
+- mostrar todos los nombres ingresados.
+- sortear un amigo secreto aleatoriamente.
+- mostrar el resultado en pantalla.
+
+---
+
+## como usarlo
+1. clonar este repositorio o descargarlo como `.zip`.
+2. abrir el archivo `index.html` en un navegador.
+3. escribir los nombres de tus amigos en el campo de texto y presionar "Añadir".
+4. cuando tengas la lista, presiona **"sortear amigo"** para elegir un ganador.
+
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let listaAmigos = [];
 
 function agregarAmigo() {
-    let verificacion = document.getElementById('amigo').value.trim();
+    let verificacion = getElemento('amigo').value.trim();
     if (verificacion === '') {
         alert('Error, El campo no puede estar vacio')
     } else {
-        listaAmigos.push(verificacion);
+        listaAmigos.push(masyusInicial(verificacion));
         limpiarCampo();
         recorrerLista();
     }
@@ -13,14 +13,39 @@ function agregarAmigo() {
 }
 
 function limpiarCampo() {
-    document.getElementById('amigo').value = '';
+    getElemento('amigo').value = '';
 }
 
+//usamos un for para obtener cada indice del tamaño total de la lista y mostrarlos cada uno
 function recorrerLista() {
-    let agregarNombre = document.getElementById('listaAmigos');
+    let agregarNombre = getElemento('listaAmigos');
     agregarNombre.innerHTML = '';
     for (let i = 0; i < listaAmigos.length; i++){
         let nombre = listaAmigos[i];
-        agregarNombre.innerHTML = `<li>${nombre}</li>`
+        agregarNombre.innerHTML += `<li>${nombre}</li>`
     }
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert('La lista de amigos está vacia');
+        return;
+    }
+    let indice = Math.floor(Math.random()* listaAmigos.length);
+    let ganador = listaAmigos[indice];
+    setElemento('resultado', `${ganador} salió sorteado`);
+} 
+
+function getElemento(id) {
+    return document.getElementById(id);
+}
+//funcion para obtener el elemento por id y asignarle un texto
+function setElemento(id, texto) {
+    let elemento = document.getElementById(id);
+    elemento.innerHTML = texto;
+    return;
+}
+//funcion para convertir una cadena en title case
+function masyusInicial(texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
 }
